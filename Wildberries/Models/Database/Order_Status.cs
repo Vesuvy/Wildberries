ff3f0388010ed59c690ea28a4e4ab18d81b691ee
baenkovav <baenkovav@206D-10>
2024-03-01 14:23:06 +0700

    Добавьте файлы проекта.

---
+++ Wildberries/Models/Database/Order_Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wildberries.Models.Database
+{
+    internal class Order_Status
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Wildberries/Models/Database/Sellers_Products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wildberries.Models.Database
+{
+    internal class Sellers_Products
+    {
+        public int Id { get; set; }
+        public Products FK_Product { get; set; }
+        public Sellers FK_Seller { get; set; }
+    }
+}
